f6ef4c38efd24e734bb0461df1bab75d47bd4584
pfc-ifsul <viniciuscabral.pl084@academico.ifsul.edu.br>
2024-12-12 17:33:02 -0300

    build decision

---
+++ ros2-software/build/robot_decision_making/CMakeFiles/ament_cmake_python_copy_robot_decision_making.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ament_cmake_python_copy_robot_decision_making"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ament_cmake_python_copy_robot_decision_making.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
